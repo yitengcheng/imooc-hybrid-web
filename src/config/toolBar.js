9a69b8de37ef92754b6ce1c6955dccd7b488e5d4
fanzehua <316679581@qq.com>
2019-10-13 15:20:02 +0800

    添加导航条配置

---
+++ src/config/toolBar.js
@@ -0,0 +1,33 @@
+// tab 按钮数据源， 在vue， react， angular MVVM框架，数据驱动视图
+module.exports = [
+    {
+        // 默认状态下的图片
+        nIcon: require('@img/home-n.svg'),
+        // 高亮状态下的图片
+        hIcon: require('@img/home-h.svg'),
+        // 名称
+        name: '首页',
+        // 组件名称
+        componentName: 'home'
+    },
+    {
+        // 默认状态下的图片
+        nIcon: require('@img/shopping-n.svg'),
+        // 高亮状态下的图片
+        hIcon: require('@img/shopping-h.svg'),
+        // 名称
+        name: '购物车',
+        // 组件名称
+        componentName: 'shopping'
+    },
+    {
+        // 默认状态下的图片
+        nIcon: require('@img/my-n.svg'),
+        // 高亮状态下的图片
+        hIcon: require('@img/my-h.svg'),
+        // 名称
+        name: '我的',
+        // 组件名称
+        componentName: 'my'
+    }
+];
