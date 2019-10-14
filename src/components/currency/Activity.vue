0d323cc9ab85a737abe4a89de1772ebd8f23e0a8
fanzehua <316679581@qq.com>
2019-10-14 10:35:45 +0800

    添加活动组件

---
+++ src/components/currency/Activity.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- 只是提供了一个空间，用来存放活动的一个内容。
+  你的活动是什么样子，我不管-->
+  <div class="activty">
+    <!-- 插槽展示活动内容 -->
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    components: {},
+    data () {
+        return {};
+    },
+    computed: {},
+    methods: {}
+};
+</script>
+<style lang='scss' scoped>
+@import "@css/style.scss";
+.activty {
+  max-height: px2rem(160);
+  position: relative;
+  z-index: 2;
+}
+</style>
