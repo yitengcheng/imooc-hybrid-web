b9ca5e7bab34b5c1114293b05776ae92a62ce3b3
fanzehua <316679581@qq.com>
2019-10-15 09:26:33 +0800

    添加功能选项模块

---
+++ src/components/currency/ModeOptions.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="mode-options">
+    <div class="mode-options-item" v-for="(item,index) in dataSource" :key="index">
+      <img class="mode-options-item-icon" :src="item.icon" />
+      <p class="mode-options-item-title">{{item.title}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import modeOptions from '@config/modeOptions.js';
+export default {
+    components: {},
+    data () {
+        return {
+            // 数据源
+            dataSource: modeOptions
+        };
+    },
+    computed: {},
+    methods: {}
+};
+</script>
+<style lang='scss' scoped>
+@import "@css/style.scss";
+.mode-options {
+  display: flex;
+  flex-wrap: wrap;
+  background-color: #fff;
+  padding-bottom: px2rem(14);
+  &-item {
+    width: 20%;
+    text-align: center;
+    margin-top: px2rem(10);
+    &-icon {
+      width: px2rem(40);
+      height: px2rem(40);
+    }
+    &-title {
+      font-size: $minInfoSize;
+      margin-top: px2rem(6);
+    }
+  }
+}
+</style>
